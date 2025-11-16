76e1f0be4f3227063c0419bd4af018855a0d62d4
CROWN6795 <yettaaj@gmail.com>
2025-11-16 22:10:11 +0100

    target blank

--- footer.php
+++ footer.php
@@ -74,7 +74,7 @@
             <svg class="w-4 h-4 mr-2 text-gold" fill="currentColor" viewBox="0 0 20 20">
               <path fill-rule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clip-rule="evenodd"/>
             </svg>
-            <a href="https://share.google/rsPZUhjkNy1HUrHZ2" class="hover:text-gold transition">Marrakech, Morocco</a>
+            <a href="https://share.google/rsPZUhjkNy1HUrHZ2" class="hover:text-gold transition" target="_blank">Marrakech, Morocco</a>
           </li>
         </ul>
       </div>
